8d857afe53bba7824b5bb61adcaa9176e10853fc
Jace <jcmillett@xactware.com>
2013-04-30 15:30:08 -0600

    Automated updates

--- modText.js
+++ modText.js
@@ -34,17 +34,17 @@ $insured_state.blur();
 $insured_state.change();
 var randomZipIndex = Math.floor(Math.random()*(10));
 var randomZip = zipCodes[randomState-1][randomZipIndex];
-var client = new XMLHttpRequest();
-client.open("GET", "http://zip.elevenbasetwo.com/v2/US/" + randomZip, true);
-client.onreadystatechange = function(){
-if(client.readyState == 4) {
-result = JSON.parse(client.responseText);
-var $insured_city=$('[name=insured_city]');
-$insured_city.val(result["city"]);
-$insured_city.blur();
-$insured_city.change();
-};
-};
+// var client = new XMLHttpRequest();
+// client.open("GET", "http://zip.elevenbasetwo.com/v2/US/" + randomZip, true);
+// client.onreadystatechange = function(){
+// if(client.readyState == 4) {
+// result = JSON.parse(client.responseText);
+// var $insured_city=$('[name=insured_city]');
+// $insured_city.val(result["city"]);
+// $insured_city.blur();
+// $insured_city.change();
+// };
+// };
 client.send();
 if(String(randomZip).length == 4){temp = randomZip;
 randomZip = "0" + temp;
