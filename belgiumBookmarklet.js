9e1ab8057621df667078f2a302f2be499c7e4648
Jace <jcmillett@xactware.com>
2013-05-08 15:48:07 -0600

    Automated updates

--- belgiumBookmarklet.js
+++ belgiumBookmarklet.js
@@ -80,10 +80,10 @@ $dataset.change();
 
 //company code
 var $company_code=$('[name=companyCode]');
-var max_companyCode = ($company_code.children('option').lengh) - 1;
+var max_companyCode = ($company_code.children('option').length) - 1;
 var random_companyCode = Math.round(Math.random()*(max_companyCode) + 1);
 $company_code.prop('selectedIndex', random_companyCode);
-$company_code.change();
+//$company_code.change();
 
 
 //Claim Info
