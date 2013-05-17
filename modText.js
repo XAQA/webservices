25f28f5f3f932d56755c34f6cc16b9305f6c9ee0
Jace <jcmillett@xactware.com>
2013-05-17 15:04:36 -0600

    Automated updates

--- modText.js
+++ modText.js
@@ -123,7 +123,7 @@ $('[name=instructions]').val('');
 
 //policy type
 var firstPolicyType = $('[name=policy_type]').prop('selectedIndex', 0).text();
-var $testInput=$('[name=client_email]').val(firstPolicyType);
+var $testInput=$('[name=instructions]').val(firstPolicyType);
 var $policy_type=$('select[name=policy_type]');
 var maxPolicyType = ($policy_type.children('option').length) - 1;
 var randomPolicyType = Math.floor(Math.random()*(maxPolicyType) + 1);
