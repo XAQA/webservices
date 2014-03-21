8a1c40b1b84a20cca20353a7a8d795eb77826e80
Jace Millett <jcmillett@xactware.com>
2014-03-21 11:52:04 -0600

    changes

--- USBookmarklet.js
+++ USBookmarklet.js
@@ -1,16 +1,4 @@
 (function(){
-
-//add utility functions to page
-var autofillScriptSource = "https://rawgithub.com/XAQA/webservices/master/USBookmarklet.js";
-var utilityScriptSource = "https://rawgithub.com/XAQA/webservices/master/formUtilityFunctions.js";
-if($("script[src='" + utilityScriptSource + "']").length == 0) {
-	var utilityScript = document.createElement("script");
-	utilityScript.src = utilityScriptSource;
-	document.getElementsByTagName("head")[0].insertBefore(utilityScript, $("script[src='" + autofillScriptSource + "']").get(0));
-	//$(utilityScript).insertBefore($("script[src='" + autofillScriptSource + "']"));
-}
-
-
 //data
 //-----------------------------------------------------------------------------------------------------------------
 var today=new Date();
@@ -165,5 +153,100 @@ setText("[name=mortgage_holder_1]", "Mortgage Holder Name");
 
 //mortgage loan number
 setText("[name=loan_number_1]", x + "-" + z + y);
+
+//Utility functions for filling out forms
+//---------------------------------------------------------------------
+//code for time objects in 12 hour format
+//you can supply hour, minute, and second offsets if you would like
+function Time(date, dHours, dMinutes, dSeconds) {
+	dHours = typeof dHours !== 'undefined' ? dHours : 0;
+	dMinutes = typeof dMinutes !== 'undefined' ? dMinutes : 0;
+	dSeconds = typeof dSeconds !== 'undefined' ? dSeconds : 0;
+
+	this.hours = 0;
+	this.minutes = 0;
+	this.seconds = 0;
+	this.ampm = "am";
+	
+	//apply offsets
+	date.setHours(date.getHours() + dHours);
+	date.setMinutes(date.getMinutes() + dMinutes);
+	date.setSeconds(date.getSeconds() + dSeconds);
+	
+	//get time in 12 hour format
+	this.minutes = date.getMinutes();
+	this.seconds = date.getSeconds();
+	var tempHour = date.getHours();
+	
+	if(tempHour == 0)
+		this.hours = 12;
+	else if(tempHour > 0 && tempHour <= 12)
+		this.hours = tempHour;
+	else {
+		this.hours = tempHour % 12;
+		this.ampm = "pm";
+	}
+}
+
+Time.prototype.toString = function()
+{
+	return (this.hours + ":" + this.minutes + ":" + this.seconds + " " + this.ampm);
+}
+
+
+//functions needed for random drop down selections
+function isElementValid(locator) {
+	var element = $(locator);
+	if(element.length > 0)
+		return true;
+	else
+		return false;
+}
+
+function selectRandomOption(selectLocator) {
+	if(isElementValid(selectLocator)) {
+		var minOption = getFirstValidOption(selectLocator);
+		var maxOption = getSelectOptionsCount(selectLocator) - 1;
+		if(maxOption > 0) {
+			selectOptionByIndex(selectLocator, getRandomInt(minOption, maxOption));
+		}
+	}
+}
+
+function selectOptionByIndex(selectLocator, index) {
+	$(selectLocator).prop("selectedIndex", index);
+	$(selectLocator).blur();
+	$(selectLocator).change();
+}
+
+function getFirstValidOption(selectLocator) {
+	var selectedOption = 0;
+	var selectElement = $(selectLocator);
+	var selectOptions = selectElement.children();
+	
+	var i = 0;
+	while($(selectOptions[i]).text() === "") {
+		selectedOption += 1;
+		i++;
+	}
+		
+	return selectedOption
+}
+
+function getSelectOptionsCount(selectLocator) {
+	return $(selectLocator).children().length;
+}
+
+//get random integer between min (inclusive) and max (inclusive)
+function getRandomInt(min, max) {
+	return Math.round(Math.random() * (max - min) + min);
+}
+
+//sets the text of the element at the locator with the text supplied
+function setText(locator, text) {
+	$(locator).val(text);
+	$(locator).blur();
+	$(locator).change();
+}
 
 })();
